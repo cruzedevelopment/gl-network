3f01ef82fd9131a0b5e1feb44574463a1436c08e
fobozongithub <147492184+cruzedevelopment@users.noreply.github.com>
2025-04-19 23:25:58 +0200

    logo

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,6 +13,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/logo.png" />
+      </head>
       <body>{children}</body>
     </html>
   )
